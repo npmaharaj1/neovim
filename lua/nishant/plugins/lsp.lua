81216f4b96baa513c914a42128c6787d2308e5ba
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-07-23 19:34:11 +1000

    fix lsp

--- lua/nishant/plugins/lsp.lua
+++ lua/nishant/plugins/lsp.lua
@@ -8,22 +8,67 @@ return {
 		'mason-org/mason.nvim',
 		'mason-org/mason-lspconfig.nvim',
 
+    "mfussenegger/nvim-jdtls",
+
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-path',
 		'hrsh7th/cmp-cmdline',
 		'hrsh7th/nvim-cmp',
 
-		-- 'L3MON4D3/LuaSnip',
-		-- 'saadparwaiz1/cmp_luasnip',
 
-		-- 'stevearc/conform.nvim',
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip',
+
+		'stevearc/conform.nvim',
 	},
 	config = function()
 		require("mason").setup({})
 		require("mason-lspconfig").setup({
-			ensure_installed = { "pyright", "html", "cssls", "quick_lint_js", "bashls", "ast_grep", "vimls", "marksman", "yamlls", "nil_ls" },
+			ensure_installed = { "pyright", "html", "cssls", "quick_lint_js", "bashls", "jdtls", "vimls", "marksman", "yamlls", "nil_ls", "lua_ls" },
 			automatic_enable = true,
+
+      handlers = {
+        function(server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup {
+            capabilities = capabilities
+          }
+        end,
+
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                format = {
+                  enable = true,
+                  defaultConfig = {
+                    indent_style = "space",
+                    indent_size = "2",
+                  }
+                },
+              }
+            }
+          }
+        end,
+
+        ["jdtls"] = function()
+          require("lspconfig").jdtls.setup {
+            capabilities = capabilities,
+            settings = {
+              java = {
+                format = {
+                  enable = true;
+                  defaultConfig = {
+                    indent_style = "space",
+                    indent_size = "4",
+                  }
+                },
+              }
+            }
+          }
+        end,
+      }
 		})
 
 		local cmp = require('cmp')
@@ -49,6 +94,28 @@ return {
 			})
 		})
 
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      }),
+      matching = { disallow_symbol_nonprefix_matching = false }
+    })
 
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+      virtual_text = true,
+    })
 	end
 }
--- lua/nishant/plugins/treesitter.lua
+++ lua/nishant/plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
         lazy = 'false',
         build = ":TSUpdate",
         config = function()
-            require('nvim-treesitter.configs').setup =({
+          local configs = require('nvim-treesitter.configs')
+            configs.setup {
                 -- A list of parser names, or "all" (the listed parsers MUST always be installed)
                 ensure_installed = {
                     "python", "java", "html", "css",
@@ -27,32 +28,11 @@ return {
                 highlight = {
                     -- `false` will disable the whole extension
                     enable = true,
-                    disable = function(lang, buf)
-                        if lang == "html" then
-                            print("disabled")
-                            return true
-                        end
 
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            vim.notify(
-                                "File larger than 100KB treesitter disabled for performance",
-                                vim.log.levels.WARN,
-                                {title = "Treesitter"}
-                            )
-                            return true
-                        end
-                    end,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
                     additional_vim_regex_highlighting = { "markdown" },
                 },
-            })
-        end
+            }
+        end,
     },
 
     {
