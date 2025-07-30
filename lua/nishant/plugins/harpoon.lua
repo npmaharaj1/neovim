33351f9e6f4901fd2f97617e25e287809eb0c0c5
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-07-30 21:10:03 +1000

    automated commit

--- lua/nishant/plugins/harpoon.lua
+++ lua/nishant/plugins/harpoon.lua
@@ -10,9 +10,9 @@ return {
 		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 		vim.keymap.set("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-		vim.keymap.set("n", "<A-n>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<A-m>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<A-,>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<A-.>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<A-u>", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<A-i>", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<A-o>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<A-p>", function() harpoon:list():select(4) end)
 	end
 }
---
+++ lua/nishant/plugins/pipeline.lua
@@ -0,0 +1,9 @@
+return {
+  'topaxi/pipeline.nvim',
+  keys = {
+    { '<leader>ci', '<cmd>Pipeline<cr>', desc = 'Open pipeline.nvim' },
+  },
+  -- optional, you can also install and use `yq` instead.
+  build = 'make',
+  -- opts = {},
+}
